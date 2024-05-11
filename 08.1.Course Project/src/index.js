dfcda2ee8dd7a3d8d6c5e68ba7299ab190650745
Kathiravan K <kathirr007@gmail.com>
2024-05-11 19:40:01 +0530

    Loaders - added css and scss loaders

--- 08.1.Course Project/src/index.js
+++ 08.1.Course Project/src/index.js
@@ -2,8 +2,11 @@ import $ from 'jquery'
 import _ from 'lodash'
 import bootstrap from 'bootstrap'
 import Typed from 'typed.js'
+import validate from 'validate.js'
+import toastr from 'toastr'
 
-import "./libs/toast/toastr.scss";
+import './_vendor.scss'
+import './index.css'
 
 const footerCol1Items = ["Home", "About", "Contact us", "FAQ", "Blog"];
 const footerCol2Items = ["Flutter", "Javascript", "Java", "Nodejs", "Python"];
--- 08.1.Course Project/webpack.config.js
+++ 08.1.Course Project/webpack.config.js
@@ -1,8 +1,25 @@
 const path = require('path')
 
 module.exports = {
-  entry: 'index.js',
+  entry: {
+    index: './src/index.js',
+    courses: './src/pages/courses.js'
+  },
   output: {
-
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader', options: { modules: true }},
+          {loader: 'sass-loader', options: { sourceMap: true }},
+        ]
+      }
+    ]
   }
 }
