e0114cea876627fe62e3eb1b44f75c4f7b761f8e
Kathiravan K <kathirr007@gmail.com>
2024-05-11 16:20:20 +0530

    05. Image loaders

---
+++ 05.Images Loaders/src/index.js
@@ -0,0 +1,34 @@
+import _ from 'lodash'
+import * as style from './index.scss'
+import './clearButton'
+import webpackLogo from './assets/awesome_webpack_branding.png'
+import avifLogo from './assets/avif-logo.webp'
+
+console.log(style)
+
+const buttonEl = document.getElementById('button1')
+const logoEl = document.getElementById('webpackLogo')
+// const avifLogoEl = document.getElementById('avifLogo')
+
+logoEl.src = webpackLogo
+// // avifLogoEl.src = avifLogo
+
+buttonEl.addEventListener('click', function() {
+    const h1El = document.getElementById('pageTitle')
+    const featuresListEl = document.getElementById('featuresList')
+
+    const listItems = ['bundling', 'tree shaking', 'compile', 'transpile']
+
+    h1El.innerHTML = 'Updated page title'
+
+    _.forEach(listItems, function(item) {
+        const liEl = document.createElement('li')
+
+        liEl.textContent = item;
+
+        featuresListEl.appendChild(liEl)
+    })
+
+})
+
+buttonEl.classList.add(['button'])
---
+++ 05.Images Loaders/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: "images/[hash][ext]",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader', options: { modules: true }},
+          {loader: 'sass-loader', options: { sourceMap: true } },
+        ]
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg|webp)/,
+        type: 'asset/resource'
+      }
+    ]
+  }
+}
